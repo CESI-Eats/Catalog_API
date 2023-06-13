3ce084eb6fae82ac93b09b169c4bcaf06508b8fb
Mathis Lemeux <mathis.lemeux@viacesi.fr>
2023-06-13 16:55:20 +0200

    Update of the model and creation of all requests for Articles

--- src/models/myModel.ts
+++ src/models/myModel.ts
@@ -1,64 +1,52 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
+
+// Define Interface
+interface Article {
+  _id: mongoose.Types.ObjectId;
+  name: String;
+  description: String;
+  image: String;
+  price: Number;
+}
+
+interface Menu {
+  _id: mongoose.Types.ObjectId;
+  name: String;
+  description: String;
+  image: String;
+  articles: [Schema.Types.ObjectId];
+}
+
+interface Catalog extends Document {
+  _id: mongoose.Types.ObjectId;
+  restorerId: mongoose.Types.ObjectId;
+  description: String;
+  image: String;
+  menus: [Menu];
+  articles: [Article];
+}
+
 
 // Define schema for Catalog
 const CatalogSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  menus: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Menu',
-  }
-});
-
-// Define schema for Menu
-const MenuSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  menus: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Articles',
-  }
-});
-
-// Define schema for Articles
-const ArticlesSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  }
+  restorerId: { type: Schema.Types.ObjectId, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  menus: [{
+    _id: { type: Schema.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    articles: [{ type: Schema.Types.ObjectId, required: true }]
+  }],
+  articles: [{
+    _id: { type: Schema.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true }
+  }]
 });
 
 // Export model
-export default mongoose.model('MyModel', CatalogSchema);
+export default mongoose.model<Catalog>('Catalog', CatalogSchema);
